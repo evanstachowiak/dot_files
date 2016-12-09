564b886f850f68d9dea32d250451a017d0be7fb1
Evan Stachowiak <evan.stachowiak@casper.com>
2016-12-08 22:29:52 -0500

    Add Brewfile support

--- setup.bash
+++ setup.bash
@@ -32,4 +32,6 @@ done
 which brew
 if ! which brew; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew tap homebrew/bundle
+  brew bundle
 fi
